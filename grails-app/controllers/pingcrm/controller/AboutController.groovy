5fc081ce56627b0373ad248db96d6faed368cac1
Mattias Reichel <mattias.reichel@gmail.com>
2023-01-03 11:51:35 +0100

    Added more info about the application on the About page.

--- grails-app/controllers/pingcrm/controller/AboutController.groovy
+++ grails-app/controllers/pingcrm/controller/AboutController.groovy
@@ -1,6 +1,8 @@
 package pingcrm.controller
 
 import grails.plugin.springsecurity.annotation.Secured
+import grails.plugins.GrailsPluginManager
+import grails.plugins.PluginManagerAware
 import grails.util.Environment
 import groovy.transform.CompileStatic
 import jakarta.inject.Inject
@@ -9,33 +11,33 @@ import pingcrm.config.GrailsInfo
 
 @CompileStatic
 @Secured('IS_AUTHENTICATED_REMEMBERED')
-class AboutController {
+class AboutController implements PluginManagerAware {
 
-    private final AppInfo appInfo
-    private final GrailsInfo grailsInfo
-    private final String grailsEnvironment
-    private final String groovyVersion
-    private final String javaVersion
+    private final Map info = [:]
 
     @Inject
     AboutController(AppInfo appInfo, GrailsInfo grailsInfo) {
-        this.appInfo = appInfo
-        this.grailsInfo = grailsInfo
-        this.grailsEnvironment = Environment.current.name
-        this.groovyVersion = GroovySystem.version
-        this.javaVersion = System.getProperty 'java.version'
+        info.with {
+            name = appInfo.name
+            version = appInfo.version
+            grailsVersion = appInfo.grailsVersion
+            grailsProfile = grailsInfo.profile
+            grailsEnvironment = Environment.current.name
+            groovyVersion = GroovySystem.version
+            javaVersion = System.getProperty 'java.version'
+        }
     }
 
     def index() {
-        def info = [
-            name: appInfo.name,
-            version: appInfo.version,
-            grailsVersion: appInfo.grailsVersion,
-            grailsProfile: grailsInfo.profile,
-            grailsEnvironment: grailsEnvironment,
-            groovyVersion: groovyVersion,
-            javaVersion: javaVersion
-        ]
         renderInertia 'About/Index', [info: info]
+    }
+
+    @Override
+    void setPluginManager(GrailsPluginManager pluginManager) {
+        info.plugins = pluginManager.allPlugins.collect {[name: it.name, version: it.version] }.sort { it.name }
+        info.numControllers = grailsApplication.getArtefacts('Controller').size()
+        info.numDomains = grailsApplication.getArtefacts('Domain').size()
+        info.numServices = grailsApplication.getArtefacts('Service').size()
+        info.numTagLibs = grailsApplication.getArtefacts('TagLib').size()
     }
 }
--- src/main/javascript/Pages/About/Index.vue
+++ src/main/javascript/Pages/About/Index.vue
@@ -11,16 +11,35 @@ const props = defineProps({
 
 <template>
   <inertia-head title="About" />
-  <h1 class="mb-8 font-bold text-3xl">About</h1>
-  <ul>
-    <li><strong>App name:</strong> {{ info['name'] }}</li>
-    <li><strong>App version:</strong> {{ info['version'] }}</li>
-    <li><strong>Grails profile:</strong> {{ info['grailsProfile'] }}</li>
-    <li><strong>Grails env:</strong> {{ info['grailsEnvironment'] }}</li>
-    <li><strong>Grails version:</strong> {{ info['grailsVersion'] }}</li>
-    <li><strong>Groovy version:</strong> {{ info['groovyVersion'] }}</li>
-    <li><strong>Java version:</strong> {{ info['javaVersion'] }}</li>
-  </ul>
+  <div class="sm:grid lg:grid-flow-col grid-cols-2 lg:grid-cols-3 lg:grid-rows-2 gap-y-4 lg:gap-y-0 xl:w-3/4 2xl:w-3/5" style="grid-template-rows: auto auto">
+    <h1 class="mb-4 sm:mb-6 lg:mb-6 font-bold text-3xl">About</h1>
+    <div>
+      <ul>
+        <li><strong>App name:</strong> {{ info['name'] }}</li>
+        <li><strong>App version:</strong> {{ info['version'] }}</li>
+        <li><strong>Grails profile:</strong> {{ info['grailsProfile'] }}</li>
+        <li><strong>Grails env:</strong> {{ info['grailsEnvironment'] }}</li>
+        <li><strong>Grails version:</strong> {{ info['grailsVersion'] }}</li>
+        <li><strong>Groovy version:</strong> {{ info['groovyVersion'] }}</li>
+        <li><strong>JVM version:</strong> {{ info['javaVersion'] }}</li>
+      </ul>
+    </div>
+    <h2 class="mt-8 sm:mt-1 mb-4 sm:mb-6 lg:mb-4 font-bold text-2xl">Artefacts</h2>
+    <div>
+      <ul>
+        <li><strong>Controllers:</strong> {{ info['numControllers'] }}</li>
+        <li><strong>Domains:</strong> {{ info['numDomains'] }}</li>
+        <li><strong>Services:</strong> {{ info['numServices'] }}</li>
+        <li><strong>Tag libraries:</strong> {{ info['numTagLibs'] }}</li>
+      </ul>
+    </div>
+    <h2 class="mt-8 sm:mt-1 mb-4 sm:mb-6 lg:mb-4 font-bold text-2xl">Installed Plugins</h2>
+    <div>
+      <ul>
+        <li v-for="plugin in info['plugins']" :key="plugin.name"><strong>{{ plugin.name }}:</strong> {{ plugin.version }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style>
