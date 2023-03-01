62fda94a802255ad77e67ca76a34baae992310d7
Mattias Reichel <mattias.reichel@gmail.com>
2023-03-01 15:30:50 +0100

    Updated vite config to use defineConfig.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default ({ command }) => ({
+export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '' : '/static/dist/',
   publicDir: false,
   build: {
@@ -28,4 +29,4 @@ export default ({ command }) => ({
       usePolling: true
     }
   }
-})
+}))
