1fe938cc684d59dc5951bb781baf4dd9f887c8e9
Mattias Reichel <mattias.reichel@gmail.com>
2023-03-08 09:35:36 +0100

    Removed PluginManagerAware interface from AboutController and inject the PluginManager in the constructor instead.

--- grails-app/controllers/pingcrm/controller/AboutController.groovy
+++ grails-app/controllers/pingcrm/controller/AboutController.groovy
@@ -15,9 +15,9 @@
  */
 package pingcrm.controller
 
+import grails.core.GrailsApplication
 import grails.plugin.springsecurity.annotation.Secured
 import grails.plugins.GrailsPluginManager
-import grails.plugins.PluginManagerAware
 import grails.util.Environment
 import groovy.transform.CompileStatic
 import jakarta.inject.Inject
@@ -32,12 +32,16 @@ import pingcrm.config.GrailsInfo
  */
 @CompileStatic
 @Secured('IS_AUTHENTICATED_REMEMBERED')
-class AboutController implements PluginManagerAware {
+class AboutController {
 
     private final Map info = [:]
 
     @Inject
-    AboutController(AppInfo appInfo, GrailsInfo grailsInfo) {
+    AboutController(AppInfo appInfo,
+                    GrailsInfo grailsInfo,
+                    GrailsApplication grailsApplication,
+                    GrailsPluginManager pluginManager)
+    {
         info.with {
             name = appInfo.name
             version = appInfo.version
@@ -46,19 +50,15 @@ class AboutController implements PluginManagerAware {
             grailsEnvironment = Environment.current.name
             groovyVersion = GroovySystem.version
             javaVersion = System.getProperty 'java.version'
+            plugins = pluginManager.allPlugins.collect {[name: it.name, version: it.version] }.sort { it.name }
+            numControllers = grailsApplication.getArtefacts('Controller').size()
+            numDomains = grailsApplication.getArtefacts('Domain').size()
+            numServices = grailsApplication.getArtefacts('Service').size()
+            numTagLibs = grailsApplication.getArtefacts('TagLib').size()
         }
     }
 
     def index() {
         renderInertia 'About/Index', [info: info]
-    }
-
-    @Override
-    void setPluginManager(GrailsPluginManager pluginManager) {
-        info.plugins = pluginManager.allPlugins.collect {[name: it.name, version: it.version] }.sort { it.name }
-        info.numControllers = grailsApplication.getArtefacts('Controller').size()
-        info.numDomains = grailsApplication.getArtefacts('Domain').size()
-        info.numServices = grailsApplication.getArtefacts('Service').size()
-        info.numTagLibs = grailsApplication.getArtefacts('TagLib').size()
     }
 }
