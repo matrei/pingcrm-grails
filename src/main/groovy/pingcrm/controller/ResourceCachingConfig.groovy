d44a90f46edbd4b14f64d87fed6820768257ad6a
Mattias Reichel <mattias.reichel@gmail.com>
2024-01-15 14:01:50 +0100

    chore: License updates

--- src/main/groovy/pingcrm/controller/ResourceCachingConfig.groovy
+++ src/main/groovy/pingcrm/controller/ResourceCachingConfig.groovy
@@ -1,3 +1,19 @@
+/*
+* Copyright 2023 original authors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package pingcrm.controller
 
 import groovy.transform.CompileStatic
@@ -8,6 +24,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 import java.util.concurrent.TimeUnit
 
+/**
+ * Configures caching headers for static resources.
+ * @author Mattias Reichel
+ * @since 3.2.0
+ */
 @Configuration
 @CompileStatic
 class ResourceCachingConfig implements WebMvcConfigurer {
