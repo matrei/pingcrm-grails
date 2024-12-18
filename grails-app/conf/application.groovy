5db263e2b2012f07bb7ee15a1ca1c7bad41e1159
Devon Deweert <25532977+DeweertD@users.noreply.github.com>
2024-12-18 12:37:35 -0700

    Addressing PR comments

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,12 +1,16 @@
 import grails.plugin.inertia.Inertia
+
+import javax.servlet.http.HttpServletRequest
+
 grails {
     plugin {
-       springsecurity {
-            ajaxHeader = "" // Disable default header based ajax matching
-            ajaxCheckClosure  = { request ->
-                // Enable inertia aware ajax: Inertia requests _LOOK_ like ajax, but are meant to be treated like standard navigation
-                if ("XMLHttpRequest" == request.getHeader("X-Requested-With")) {
-                    return !(request.getHeader(Inertia.INERTIA_HEADER) as Boolean)
+        springsecurity {
+            ajaxHeader = '' // Disable default header based ajax matching
+            ajaxCheckClosure = { HttpServletRequest request ->
+                /** Enable inertia aware ajax:
+                 * Inertia requests _LOOK_ like ajax, but are meant to be treated like standard navigation */
+                if ('XMLHttpRequest' == request.getHeader('X-Requested-With')) {
+                    return !Inertia.isInertiaRequest
                 }
                 return false
             }
